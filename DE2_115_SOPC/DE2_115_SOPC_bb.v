1d54a2e7345543f17653c6a65562468b6a15a1d7
stiangh <32125631+stiangh@users.noreply.github.com>
2022-03-18 14:10:28 +0100

    added spi module to nios 2 and connected to gpio pins

--- DE2_115_SOPC/DE2_115_SOPC_bb.v
+++ DE2_115_SOPC/DE2_115_SOPC_bb.v
@@ -1,10 +1,18 @@
 
 module DE2_115_SOPC (
 	clk_clk,
+	pio_led_external_connection_export,
 	reset_reset_n,
-	pio_led_external_connection_export);	
+	spi_led_matrix_external_MISO,
+	spi_led_matrix_external_MOSI,
+	spi_led_matrix_external_SCLK,
+	spi_led_matrix_external_SS_n);	
 
 	input		clk_clk;
-	input		reset_reset_n;
 	output	[7:0]	pio_led_external_connection_export;
+	input		reset_reset_n;
+	input		spi_led_matrix_external_MISO;
+	output		spi_led_matrix_external_MOSI;
+	output		spi_led_matrix_external_SCLK;
+	output		spi_led_matrix_external_SS_n;
 endmodule
--- DE2_115_SOPC/DE2_115_SOPC_inst.vhd
+++ DE2_115_SOPC/DE2_115_SOPC_inst.vhd
@@ -1,15 +1,23 @@
 	component DE2_115_SOPC is
 		port (
 			clk_clk                            : in  std_logic                    := 'X'; -- clk
+			pio_led_external_connection_export : out std_logic_vector(7 downto 0);        -- export
 			reset_reset_n                      : in  std_logic                    := 'X'; -- reset_n
-			pio_led_external_connection_export : out std_logic_vector(7 downto 0)         -- export
+			spi_led_matrix_external_MISO       : in  std_logic                    := 'X'; -- MISO
+			spi_led_matrix_external_MOSI       : out std_logic;                           -- MOSI
+			spi_led_matrix_external_SCLK       : out std_logic;                           -- SCLK
+			spi_led_matrix_external_SS_n       : out std_logic                            -- SS_n
 		);
 	end component DE2_115_SOPC;
 
 	u0 : component DE2_115_SOPC
 		port map (
 			clk_clk                            => CONNECTED_TO_clk_clk,                            --                         clk.clk
+			pio_led_external_connection_export => CONNECTED_TO_pio_led_external_connection_export, -- pio_led_external_connection.export
 			reset_reset_n                      => CONNECTED_TO_reset_reset_n,                      --                       reset.reset_n
-			pio_led_external_connection_export => CONNECTED_TO_pio_led_external_connection_export  -- pio_led_external_connection.export
+			spi_led_matrix_external_MISO       => CONNECTED_TO_spi_led_matrix_external_MISO,       --     spi_led_matrix_external.MISO
+			spi_led_matrix_external_MOSI       => CONNECTED_TO_spi_led_matrix_external_MOSI,       --                            .MOSI
+			spi_led_matrix_external_SCLK       => CONNECTED_TO_spi_led_matrix_external_SCLK,       --                            .SCLK
+			spi_led_matrix_external_SS_n       => CONNECTED_TO_spi_led_matrix_external_SS_n        --                            .SS_n
 		);
 
