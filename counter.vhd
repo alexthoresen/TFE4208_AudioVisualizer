2d25d1136cfbda41722f8475636424ed4e799a5b
stiangh <32125631+stiangh@users.noreply.github.com>
2022-05-04 14:43:20 +0200

    added counter and keep_max implementations

--- counter.vhd
+++ counter.vhd
@@ -4,7 +4,9 @@ use ieee.numeric_std.all;
 
 -- Counter for filter bank
 entity counter is
-
+    generic (
+        SIZE_BITS : integer
+    );
     port (INCREMENT : in STD_LOGIC;
           CLK       : in STD_LOGIC;
           RESET_N   : in STD_LOGIC;
@@ -15,24 +17,61 @@ entity counter is
 end counter;
 
 
-architecture fsm_arch of counter is
+architecture rtl of counter is
 
--- constant IDLE    : STD_LOGIC := '0';
--- constant OPERATE : STD_LOGIC := '1';
+    signal count_state : std_logic_vector(SIZE_BITS-1 downto 0);
+    signal next_state : std_logic_vector(SIZE_BITS-1 downto 0);
+    signal up_vector : std_logic_vector(SIZE_BITS-1 downto 0);
+    signal and_vector : std_logic_vector(SIZE_BITS-1 downto 0);
 
--- signal state: STD_LOGIC;
-
-signal counter : UNSIGNED (15 DOWNTO 0);
-
+    signal overflow_reg : std_logic;
 begin
-
-
-    process (CLK) is begin
-
-        if (CLK'event and CLK = '1') then
-        -- Blank
+    
+    P_COUNTER_REGS: process(CLK, RESET_N)
+    begin
+        if RESET_N = '0' then
+            count_state <= (others => '0');
+        elsif rising_edge(CLK) then
+            count_state <= next_state;
         end if;
+    end process P_COUNTER_REGS;
 
-    end process;
+    P_NEXT: process(count_state, up_vector, INCREMENT)
+    begin
+        if INCREMENT = '1' then
+            next_state <= up_vector;
+        else
+            next_state <= count_state;
+        end if;
+    end process P_NEXT;
 
-end fsm_arch;
+    P_UP_VECTOR: process(count_state, and_vector)
+    begin
+        up_vector(0) <= not count_state(0);
+        for I in 1 to up_vector'high loop
+            up_vector(I) <= count_state(I) xor and_vector(I-1);
+        end loop;
+    end process P_UP_VECTOR;
+
+    P_AND_VECTOR: process(and_vector, count_state)
+    begin
+        and_vector(0) <= count_state(0);
+        for I in 1 to and_vector'high loop
+            and_vector(I) <= count_state(I) and and_vector(I-1);
+        end loop;
+    end process P_AND_VECTOR;
+
+    P_OVERFLOW_REG: process(CLK, RESET_N)
+    begin
+        if RESET_N = '0' then
+            overflow_reg <= '0';
+        elsif rising_edge(CLK) then
+            overflow_reg <= INCREMENT and and_vector(and_vector'high);
+        end if;
+    end process P_OVERFLOW_REG;
+
+    -- OUTPUTS
+
+    COUNTER_DONE <= overflow_reg;
+    
+end architecture rtl;
---
+++ keep_max.vhd
@@ -0,0 +1,54 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+entity keep_max is
+  Generic (
+    SIZE_BITS : integer
+  );
+  Port (
+    CLK : in std_logic;
+    RESET_N : in std_logic;
+
+    A : in std_logic_vector(SIZE_BITS-1 downto 0);
+    
+    STORE : in std_logic;
+    FORCE_NEW : in std_logic;
+    
+    O : out std_logic_vector(SIZE_BITS-1 downto 0)
+  );
+end keep_max;
+
+architecture rtl of keep_max is
+    signal is_larger : std_logic;
+    signal reg : std_logic_vector(SIZE_BITS-1 downto 0);
+begin
+
+    P_CMP: process(A, reg)
+    begin
+        if unsigned(A) > unsigned(reg) then
+            is_larger <= '1';
+        else
+            is_larger <= '0';
+        end if;
+    end process P_CMP;
+
+    P_REG: process(CLK, RESET_N)
+    begin
+        if RESET_N = '0' then
+            reg <= (others => '0');
+        elsif rising_edge(CLK) then
+            if FORCE_NEW = '1' or (STORE = '1' and is_larger = '1') then
+                reg <= A;
+            end if;
+        end if;
+    end process P_REG;
+
+    -- OUTPUTS
+
+    O <= reg;
+
+end rtl;
